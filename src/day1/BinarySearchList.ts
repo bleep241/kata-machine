25920f8e5bbff7226166431044a52a2087c9f984
Billy Lee <ucanfindbillie@gmail.com>
2024-06-23 13:01:14 -0700

    implemented binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0;
+  let hi = haystack.length;
 
+  do {
+    let mid = (lo + (hi - lo) / 2);
+    let val = haystack[mid];
+
+    if (val === needle) {
+      return true;
+    } else if (val > needle) {
+      hi = mid;
+    } else {
+      lo = mid + 1;
+    }
+  } while (lo < hi);
+
+  return false;
 }
+
+console.log(bs_list([2,3,5,6,8,9], 5));
