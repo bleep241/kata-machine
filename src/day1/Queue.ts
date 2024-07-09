adaef84ed0e42c799feec16bfd4c80c66581de68
Billy Lee <ucanfindbillie@gmail.com>
2024-07-08 17:34:43 -0700

    implemented a stack

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -14,39 +14,43 @@ export default class Queue<T> {
   }
 
   enqueue(item: T): void {
-    // A -> B -> C adding an item -> D
-    const newNode = {
+    const newNode = { // new node to be added
       value: item,
     } as QNode<T>;
-
+    
     // need to have the tail point to D
     // need to have C.next point to D
-    this.length++;
+    this.length++; // increases length of the queue
     if (!this.tail) {
       this.tail = this.head = newNode; // if there is no tail then there is no head so point both to the new node
+    } else {
+      // A -> B -> C adding an item -> D
+      this.tail.next = newNode; // this there is a tail then point the current tail to the next node
+      this.tail = newNode; // make our new node the tail
     }
-
-    this.tail.next = newNode; // this there is a tail then point the current tail to the next node
-    this.tail = newNode; // make our new node the tail
+    
   }
-  dequeue(): T | undefined {
-    if (!this.head) {
-      return undefined;
-    }
-    // A -> B -> C
-    this.length--;
 
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined; // if there is no node on the queue return undefined
+    }
+
+    this.length--; 
+    
+    // A -> B -> C
     const head = this.head; // save a reference to the head
     this.head = this.head.next; // point the head of the queue to the node that is next in line to the head
-    head.next = undefined;
+    head.next = undefined; // removing reference to the previous head to effectively 'take them out of the queue/line'
 
     if (this.length === 0) {
-      this.tail = undefined;
+      this.tail = this.head = undefined; // if there are no nodes then there is no tail
     }
 
     return head.value;
   }
+
   peek(): T | undefined {
-    return this.head?.value;
+    return this.head?.value; // get the value of the head and check if it exists ? return value of the head : return undefined
   }
 }
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,48 @@
-export default class Stack<T> {
-    public length: number;
+type Node<T> = {
+  value: T,
+  next?: Node<T>,
+}
 
-    
+export default class Stack<T> { //Last in First Out or LiFO data structure
+  public length: number;
+  private head?: Node<T>;
 
-    constructor() {
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+  }
+
+  // A <- B <- C <- D <+- (E)
+  push(item: T): void {
+    const newNode: Node<T> = {
+      value: item,
+    };
+
+    this.length++;
+
+    if (!this.head) {
+      this.head = newNode;
     }
 
-    push(item: T): void {
+    newNode.next = this.head; // E node's next now points to previous head
+    this.head = newNode; // stack's head now points to E
+  }
+  pop(): T | undefined { // save reference to the current head, make the stack's head next point to the new head, return the saved head's value
+    if (!this.head) return undefined;
 
-}
-    pop(): T | undefined {
+    this.length--;
 
-}
-    peek(): T | undefined {
+    const head = this.head;
+    this.head = this.head.next;
+    head.next = undefined;
 
-}
+    if (this.length === 0) {
+      this.head = undefined; // if there are no nodes left then there is no head
+    }
+
+    return head.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
